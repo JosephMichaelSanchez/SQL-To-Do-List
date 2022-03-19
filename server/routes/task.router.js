8186134e62b8cab03f8fcb2103ee3d6e81884f3d
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-18 19:51:20 -0500

    Set up router

---
+++ server/routes/task.router.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const taskRouter = express.Router();
+
+// DB CONNECTION
+const pool = require('../modules/pool.js')
+
+// GET
+taskRouter.get('/', (req, res) => {
+    console.log('in GET');
+    let queryText = `
+        SELECT * FROM "tasks"
+    `;
+
+    pool.query(queryText).then(tasks => {
+        res.send(tasks.rows);
+    }).catch(error => {
+        console.log('Error getting tasks', error);
+        res.sendStatus(500);
+    })
+
+})
+
+// POST
+taskRouter.post('/', (req, res) => {
+    let newTask = req.body
+    console.log('Adding', newTask);
+
+
+    // convert readyForTransfer to ready_to_transfer
+    //  if(newKoala.ready_to_transfer.toLowerCase() === 'yes') {
+    //     newKoala.ready_to_transfer = true;
+    //   } else if(newKoala.ready_to_transfer.toLowerCase() === 'no') {
+    //     newKoala.ready_to_transfer = false;
+    //   } else {
+    //     alert('Please say yes or no for ready to transfer <3')
+    //   }
+
+    let queryText = `
+    INSERT INTO "tasks"
+    ("task", "description", "status")
+    VALUES ($1, $2, $3);
+    `;
+
+    let values = [newTask.task, newTask.description, newTask.status]
+    
+    pool.query(queryText, values)
+    .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding new task`, error);
+        res.sendStatus(500);
+      });
+})
+
+
+//PUT
+
+taskRouter.put('/:id', (req, res) => {
+    console.log(req.params.id);
+    let queryText = `
+    UPDATE "tasks"
+    SET "status" = TRUE
+    WHERE "id" = $1;
+    
+    `;
+
+    const values = [req.params.id];
+    pool.query(queryText, values)
+        .then(result => {
+            res.sendStatus(200);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+//DELETE
+
+taskRouter.delete('/:id', (req, res) => {
+    console.log('delete a task', req.params.id);
+    let id = req.params.id;
+    const queryText = `
+    DELETE FROM "tasks"
+    WHERE "id" = $1;
+    `;
+
+    const values = [id];
+
+    pool.query(queryText,values)
+    .then(result => {
+        res.sendStatus(204);
+    }).catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+});
+
+
+
+
+module.exports = taskRouter;
