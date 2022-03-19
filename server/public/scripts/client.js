b727cddf3b656dc15c81c61e142656b16a0c1ba3
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-18 20:09:55 -0500

    Add click listeners to ready function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,16 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
+
+  // click-listener for the ADD TASK button, will call that function
+$('#addTaskBtn').on('click', addTask);
+
+// click listener for the MARK COMPLETED buttons, will call markCompleted function
+$('body').on('click', '.markCompletedBtn', markCompleted);
+
+// click-listener for the DELETE buttons
+$('body').on('click', '.deleteBtn', deleteTask);
+
+getTasks();
+
 }); // end doc ready
